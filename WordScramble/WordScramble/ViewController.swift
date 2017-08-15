a7b2726493ad8185d9b5c5df22d6e03e1d495ee6
Jose Melendez <JoseM_Melendez@icloud.com>
2017-08-15 17:51:53 -0400

    Organized project, ready to begin

---
+++ WordScramble/WordScramble/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordScramble
+//
+//  Created by Jose Melendez on 8/15/17.
+//  Copyright © 2017 JoseMelendez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

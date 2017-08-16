26b20253621257dfd68658223e327f4c0e4f7db1
Jose Melendez <JoseM_Melendez@icloud.com>
2017-08-15 21:06:21 -0400

    Added the startGame function

--- WordScramble/WordScramble/ViewController.swift
+++ WordScramble/WordScramble/ViewController.swift
@@ -7,14 +7,52 @@
 //
 
 import UIKit
+//Import GamePlayKit so that we can use the shuffle method in the startGame() function
+import GameplayKit
 
 class ViewController: UITableViewController{
+    
+    //Array of all the words we have
+    var allWords = [String]()
+    
+    //Array of all the words the user has used
+    var usedWords = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        /* START. Fill the allWords Array with the words in the start.txt file */
+        
+        if let startWordsPath = Bundle.main.path(forResource: "start", ofType: "txt"){
+            
+            if let startWords = try? String(contentsOfFile: startWordsPath) {
+                
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        } else {
+            
+            allWords = ["placeHolder"]
+        }
+        
+        /* END. --------*/
+        
+        startGame()
     }
-
+    
+    //MARK: startGame function
+    
+    func startGame() {
+        
+        //Randomize the words in allWords and return the randomized array of strings
+        allWords = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: allWords) as! [String]
+        
+        //Sets our view controllers title
+        title = allWords[0]
+        
+        usedWords.removeAll(keepingCapacity: true)
+        
+        tableView.reloadData()
+    }
 
 }
 
